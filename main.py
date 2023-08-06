f87d28618f189ad7262411d1755be5bfcaa9acad
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-06 11:16:46 +0545

    add

---
+++ main.py
@@ -0,0 +1,110 @@
+a = 10
+number = "Digital";
+
+# weight = int(input("Enter your weight"))
+# answer = input("Do you want to change in pound(p) or kg(k)").upper()
+# result = 0;
+
+#check answer here using if else condition
+
+# if(answer == "P" ) : 
+#     result = weight * 1.2
+    
+# else : 
+#     result = weight * 9
+
+# print(result)
+
+
+
+#print(a + number)
+#print(float(a))
+
+#array example
+# arrayofNumbers = [1,2,3,4,5,6]
+#print(arrayofNumbers[-2])
+
+#to add to the end of array
+#arrayofNumbers.append(7)
+
+#loop
+#for number in arrayofNumbers : 
+    #print(number)
+
+#personInfo = {"name":"Manish"}
+
+# Range 
+
+# for i in range(0,5):
+#     print(i)
+
+#Insert range value to array and also insert(append) 5 at the end
+
+# arr = []
+# for i in range(0,4):
+#     arr.append(i)
+
+# arr.append(5)
+# print(arr)
+
+# Print minimum value of array
+
+
+# Get the length of oddNumbers and jati aauxa length teti ko range numbers new array ma halnu paryo
+
+# lengthOfOddNumbers  = len(oddNumbers)
+# newArray = []
+# for i in range(0,lengthOfOddNumbers) :
+#     newArray.append(i)
+
+# print(newArray)
+
+# 1 dekhi 20 samma oddNumber nikalnu paryo
+
+# numbers = [1,2,3,4,5,6,7,9]
+
+# def test(num):
+#     return num % 2 == 0
+
+# result = list(filter(test,numbers))
+
+# print(result)
+
+#OddNumbers ko array ko divisible by 3 nikalnu paryo   
+
+# def square(num):
+#     return num **2
+
+
+# squaredList = list(map(square,result))
+
+# print(squaredList)
+
+# array = [1,2,3,4,5,6]
+# arrayOfNames = ["Manish","Prakash","Hello"]
+
+# for i,name in enumerate(arrayOfNames):
+#    print({i : name })
+ 
+# firstArray = [1,2,3]
+# secondArray = [4,5,6,7,8]
+
+# for f,s in zip(firstArray,secondArray):
+#    print(f,s)
+
+# function to calculate sum 
+
+# def sum(a,b):
+#     return a+b
+
+# result = sum(1,2)
+# print(result)
+
+#Tuples(Immutable in nature)
+tuplesExample = (1,"Hello","world",True)
+
+# print(tuplesExample[0])
+
+#Error throws garxa reassign garna milena
+tuplesExample[0] = 2
+print(tuplesExample)
